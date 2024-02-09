90d0164db0de717bfca1f237ea4eb2ee96136b15
simon lehericey <mail@simon.lehericey.net>
2024-02-09 14:42:07 +0100

    Use custom fetcher to catch assets errors while generating pdf

---
+++ custom_fetcher.py
@@ -0,0 +1,13 @@
+from weasyprint.urls import default_url_fetcher
+from urllib.error import URLError
+
+
+def custom_url_fetcher(url, timeout=10, ssl_context=None):
+    try:
+        result = default_url_fetcher(url, timeout, ssl_context)
+    except URLError:
+        # Ugly hack:
+        # the caller swallows all `Exception`
+        # but raise an AttributeError if the result is None
+        result = None
+    return result
