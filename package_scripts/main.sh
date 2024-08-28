c188bff3bcb9700ebec8bd5f4397feab02fafeee
simon lehericey <mail@simon.lehericey.net>
2024-08-28 21:59:27 +0200

    remove weasyprint.ini, add default var in service, use env conf file

--- package_scripts/main.sh
+++ package_scripts/main.sh
@@ -29,11 +29,11 @@ docker cp builder:$(docker exec builder sh -c 'find $(pwd) -iname weasyprint_ser
 # run
 docker build -t vanilla -f Dockerfile_vanilla .
 
-docker run -p 8000:8000 -dit --name vanilla vanilla:latest /bin/bash
+docker run -p 8000:8000 -p 9191:9191 -dit --name vanilla vanilla:latest /bin/bash
 
 LAST_DEB=$(find tmp -iname '*.deb' | sort | tail -n 1)
 docker cp $LAST_DEB vanilla:/root
 
 docker exec vanilla dpkg -i /root/$(basename $LAST_DEB)
 
-docker exec -u weasyprint vanilla /bin/bash -c "LOG_DIR=/var/log/weasyprint BASE_URL=$BASE_URL /opt/weasyprint/app/.venv/bin/uwsgi --ini /etc/opt/weasyprint/weasyprint.ini"
+docker exec -u weasyprint vanilla /bin/bash -c "LOG_DIR=/var/log/weasyprint BASE_URL=$BASE_URL UWSGI_HTTP_SOCKET=0.0.0.0:8000 UWSGI_STATS=0.0.0.0:9191 UWSGI_PROCESSES=4 UWSGI_ENABLE_THREADS=true UWSGI_MODULE=wsgi:app UWSGI_CHDIR=/opt/weasyprint/app /opt/weasyprint/app/.venv/bin/uwsgi"
